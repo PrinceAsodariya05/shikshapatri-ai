9a600ed60e82860fb96a491f7f6ce53c03725141
Prince Asodariya <asodariyaprince05@gmail.com>
2026-06-12 13:49:51 +0530

    Update Chatapp.jsx

--- client/src/component/Chatapp.jsx
+++ client/src/component/Chatapp.jsx
@@ -18,7 +18,7 @@ if (!question.trim()) return;
 try {
   setLoading(true);
 
-  const res = await axios.post("https://shikshapatri-ai-server.onrender.com", {
+  const res = await axios.post("https://shikshapatri-ai-server.onrender.com/ask", {
     question,
   });
 
